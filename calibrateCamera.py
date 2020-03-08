0520bccc1e590a49ffcbac2548e1a586cb90266e
Vern Francisco <avernethy@gmail.com>
2020-03-08 15:04:15 -0700

    add section to save camera calibrations

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -1,10 +1,16 @@
 import cv2
 import numpy as np
+import pickle
 
+#undistort the images
+def undistort(gray_img,mtx,dist):
+    undst = cv2.undistort(gray_img, mtx, dist, None, mtx)
+    return undst
+
+#Capture images by using the spacebar to get new images, ESC to finish capturing
 camera = cv2.VideoCapture(1)
 
 images = []
-
 img_counter = 0
 
 while True:
@@ -30,13 +36,13 @@ while True:
 camera.release()
 cv2.destroyAllWindows()
 
+# find the checkerboard pattern in the images
 imgpoints = []
 objpoints = []
 nx = 7
 ny = 5
 objp = np.zeros((nx*ny,3), np.float32)
 objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
-
 
 for image in images:
     #cv2.namedWindow("window")
@@ -63,14 +69,18 @@ for image in images:
 #Calibrate the camera
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-#undistort the images
-def undistort(gray_img,mtx,dist):
-    undst = cv2.undistort(gray_img, mtx, dist, None, mtx)
-    return undst
-
+#Show the indistorted images
 for image in images:
     test_img_undist = undistort(np.copy(image),mtx,dist)
 
     cv2.imshow('Undistorted Image', test_img_undist)
 
     cv2.waitKey(0)
+
+#Save the calibration values for usage with the project
+with open('cameraCals.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump([ret, mtx, dist, rvecs, tvecs], f)
+
+# Getting back the objects:
+#with open('objs.pkl') as f:  # Python 3: open(..., 'rb')
+#    obj0, obj1, obj2 = pickle.load(f)
